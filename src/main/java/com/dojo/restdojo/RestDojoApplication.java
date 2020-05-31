b4df600189b5b8b3e9aaa18e71c6ab224838aa65
Nicholas Ocket <nicholasocket@C02SY03ZFVH4-Nicholas-Ocket.local>
2020-05-31 21:04:35 +0200

    setting up the dojo

---
+++ src/main/java/com/dojo/restdojo/RestDojoApplication.java
@@ -0,0 +1,13 @@
+package com.dojo.restdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDojoApplication.class, args);
+	}
+
+}
